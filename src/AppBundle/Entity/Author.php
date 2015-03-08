3e4a073b760d6df6b9846803584eca05f070d958
Glynn Forrest <me@glynnforrest.com>
2015-03-08 13:34:35 +0000

    Creating Book and Author entities.

---
+++ src/AppBundle/Entity/Author.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Author
+ */
+class Author
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $forename;
+
+    /**
+     * @var string
+     */
+    private $surname;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set forename
+     *
+     * @param string $forename
+     * @return Author
+     */
+    public function setForename($forename)
+    {
+        $this->forename = $forename;
+
+        return $this;
+    }
+
+    /**
+     * Get forename
+     *
+     * @return string 
+     */
+    public function getForename()
+    {
+        return $this->forename;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     * @return Author
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string 
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+}
---
+++ src/AppBundle/Entity/Book.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Book
+ */
+class Book
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $synopsis;
+
+    /**
+     * @var integer
+     */
+    private $pages;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param  string $title
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set synopsis
+     *
+     * @param  string $synopsis
+     * @return Book
+     */
+    public function setSynopsis($synopsis)
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    /**
+     * Get synopsis
+     *
+     * @return string
+     */
+    public function getSynopsis()
+    {
+        return $this->synopsis;
+    }
+
+    /**
+     * Set pages
+     *
+     * @param  integer $pages
+     * @return Book
+     */
+    public function setPages($pages)
+    {
+        $this->pages = $pages;
+
+        return $this;
+    }
+
+    /**
+     * Get pages
+     *
+     * @return integer
+     */
+    public function getPages()
+    {
+        return $this->pages;
+    }
+}
