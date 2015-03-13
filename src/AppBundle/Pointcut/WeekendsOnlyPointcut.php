a96415f957bd556df8915a5c324e8fe245f63515
Glynn Forrest <me@glynnforrest.com>
2015-03-13 18:44:31 +0000

    Add WeekendsOnlyPointcut.

---
+++ src/AppBundle/Pointcut/WeekendsOnlyPointcut.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Pointcut;
+
+use JMS\AopBundle\Aop\PointcutInterface;
+use Doctrine\Common\Annotations\Reader;
+
+/**
+ * WeekendsOnlyPointcut
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class WeekendsOnlyPointcut implements PointcutInterface
+{
+    public function __construct(Reader $reader)
+    {
+        $this->reader = $reader;
+    }
+
+    public function matchesClass(\ReflectionClass $class)
+    {
+        //match all classes
+        return true;
+    }
+
+    public function matchesMethod(\ReflectionMethod $method)
+    {
+        //match methods with the Mischief annotation
+        return null !== $this->reader->getMethodAnnotation($method, 'AppBundle\Annotation\WeekendsOnly');
+    }
+}
